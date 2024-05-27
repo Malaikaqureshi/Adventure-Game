728cf65b53192083f588926bebb7937278c6ffe5
Malaikaqureshi <malaikaqureshi9@gmail.com>
2024-05-28 00:49:37 +0500

    Adding Adventure Game

---
+++ main.ts
@@ -0,0 +1,209 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+//Print Message
+console.log(chalk.blue.bold("\n \t Welcome to Code With Malaika Adventure-Game \n\t"));
+
+class Player{
+    name:string;
+    fuel:number = 100;
+    constructor(name:string){
+        this.name = name;
+    }
+    fuelDecrease(){
+        let fuel = this.fuel - 25
+        this.fuel = fuel
+    }
+    fuelIncrease(){
+        this.fuel = 100
+    }
+
+}
+
+
+class Opponent{
+    name:string;
+    fuel:number = 100;
+    constructor(name:string){
+        this.name = name;
+    }
+    fuelDecrease(){
+        let fuel = this.fuel - 25
+        this.fuel = fuel
+    }
+
+}
+
+let player = await inquirer.prompt([
+    {
+        name: "name",
+        type: "input",
+        message: "Please Enter Your Name: "
+
+    }
+])
+
+let opponent = await inquirer.prompt([
+    {
+        name: "select",
+        type: "list",
+        message: "Select Your Opponent",
+        choices: ["Skeleton", "Alien", "Zombie"]
+    }
+])
+
+let p1 = new Player(player.name)
+let o1 = new Opponent(opponent.select)
+
+do{
+    //Skeleton
+    if(opponent.select == "Skeleton"){
+        let ask = await inquirer.prompt([
+            {
+                name: "opt",
+                type: "list",
+                message: "What would you like to do?",
+                choices: ["Attack", "Drink Portion", "Run For Your Life.."]
+            }
+        ]);
+        if(ask.opt === "Attack"){
+            let num = Math.floor(Math.random() * 2)
+            if(num > 0){
+                p1.fuelDecrease()
+                    console.log(`${p1.name} fuel is ${p1.fuel}`);
+                    console.log(`${o1.name} fuel is ${o1.fuel}`);
+                    if(p1.fuel <= 0){
+                        console.log(chalk.red("\n You Loose, Better Luck Next Time\n"));
+                        process.exit()
+                    }
+                
+            }
+            if(num <= 0) {
+                o1.fuelDecrease()
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+                console.log(`${o1.name} fuel is ${o1.fuel}`)
+                if(o1.fuel <= 0){
+                    console.log(chalk.green("\n You Win\n"));
+                    process.exit()
+                }
+                
+
+            }
+        }
+        if(ask.opt == "Drink Portion"){
+            p1.fuelIncrease()
+            console.log(chalk.magenta(`\n You Drink Health Portion Your Fuel is ${p1.fuel} \n`));
+
+        }
+        if(ask.opt == "Run For Your Life.."){
+            console.log(chalk.red("\n You Loose, Better Luck Next Time\n"));
+            process.exit()
+
+        }
+        
+    
+        
+    }
+
+    //Alien
+    if(opponent.select == "Alien"){
+        let ask = await inquirer.prompt([
+            {
+                name: "opt",
+                type: "list",
+                message: "What would you like to do?",
+                choices: ["Attack", "Drink Portion", "Run For Your Life.."]
+            }
+        ]);
+        if(ask.opt === "Attack"){
+            let num = Math.floor(Math.random() * 2)
+            if(num > 0){
+                p1.fuelDecrease()
+                    console.log(`${p1.name} fuel is ${p1.fuel}`);
+                    console.log(`${o1.name} fuel is ${o1.fuel}`);
+                    if(p1.fuel <= 0){
+                        console.log(chalk.red("\n You Loose, Better Luck Next Time\n"));
+                        process.exit()
+                    }
+                
+            }
+            if(num <= 0) {
+                o1.fuelDecrease()
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+                console.log(`${o1.name} fuel is ${o1.fuel}`)
+                if(o1.fuel <= 0){
+                    console.log(chalk.green("\n You Win\n"));
+                    process.exit()
+                }
+                
+
+            }
+        }
+        if(ask.opt == "Drink Portion"){
+            p1.fuelIncrease()
+            console.log(chalk.magenta(`\n You Drink Health Portion Your Fuel is ${p1.fuel}\n`));
+
+        }
+        if(ask.opt == "Run For Your Life.."){
+            console.log(chalk.red("\n You Loose, Better Luck Next Time\n"));
+            process.exit()
+
+        }
+        
+    
+        
+    }
+
+    //Zombie
+    if(opponent.select == "Zombie"){
+        let ask = await inquirer.prompt([
+            {
+                name: "opt",
+                type: "list",
+                message: "What would you like to do?",
+                choices: ["Attack", "Drink Portion", "Run For Your Life.."]
+            }
+        ]);
+        if(ask.opt === "Attack"){
+            let num = Math.floor(Math.random() * 2)
+            if(num > 0){
+                p1.fuelDecrease()
+                    console.log(`${p1.name} fuel is ${p1.fuel}`);
+                    console.log(`${o1.name} fuel is ${o1.fuel}`);
+                    if(p1.fuel <= 0){
+                        console.log(chalk.red("\n You Loose, Better Luck Next Time\n"));
+                        process.exit()
+                    }
+                
+            }
+            if(num <= 0) {
+                o1.fuelDecrease()
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+                console.log(`${o1.name} fuel is ${o1.fuel}`)
+                if(o1.fuel <= 0){
+                    console.log(chalk.green("\n You Win\n"));
+                    process.exit()
+                }
+                
+
+            }
+        }
+        if(ask.opt == "Drink Portion"){
+            p1.fuelIncrease()
+            console.log(chalk.magenta(`\n You Drink Health Portion Your Fuel is ${p1.fuel}\n`));
+
+        }
+        if(ask.opt == "Run For Your Life.."){
+            console.log(chalk.red("\nYou Loose, Better Luck Next Time"));
+            process.exit()
+
+        }
+        
+    
+        
+    }
+    
+}
+
+while(true)
